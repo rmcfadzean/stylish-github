199a9b6ee76105aa46755f2618edef5c107526de
Rob McFadzean <root@sphericalcube.net>
2017-02-06 05:59:57 +1030

    New repo who dis

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+      serveStatic: [{
+        route: '/custom',
+        dir: './css'
+      }],
+      proxy: {
+        target: "https://github.com",
+        proxyRes: [
+          function (proxyRes, req, res) {
+            delete proxyRes.headers['content-security-policy'];
+          }
+        ]
+      },
+      open: false,
+      cors: true,
+      snippetOptions: {
+        rule: {
+          match: /<\/body>/i,
+          fn: function (snippet, match) {
+            return '<link rel="stylesheet" type="text/css" href="custom/style.css"/>' + snippet + match;
+          }
+        }
+      }
+    });
+
+    gulp.watch("scss/*.scss", ['sass']);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("scss/style.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
